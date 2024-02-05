bbe2c38c4f65f088efdaa5bd1419de8581ec3532
Erwin Knoop <erwin@chargee.energy>
2024-02-05 12:06:32 +0100

    Bar chart minY fix

--- lib/bar/models/bar_chart_data.dart
+++ lib/bar/models/bar_chart_data.dart
@@ -5,14 +5,8 @@ import 'bar_chart_item.dart';
 
 /// An implementation of [ChartData] specifically used for a bar chart.
 class BarChartData extends ChartData<BarChartItem> {
-  /// Whether the chart has positive bars
-  bool get hasPositive => items.any((item) => item.positiveStack != null);
-
-  /// Whether the chart has negative bars
-  bool get hasNegative => items.any((item) => item.negativeStack != null);
-
   @override
-  double get minY => items.map((item) => item.minY).maxOrNull ?? 0;
+  double get minY => -(items.map((item) => item.minY).maxOrNull ?? 0);
 
   @override
   double get maxY => items.map((item) => item.maxY).maxOrNull ?? 0;
